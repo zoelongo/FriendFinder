2c4ffbaef815bc597a0fc286fea66be3bd8073e5
Zoe Longo <zoelongo@yahoo.com>
2019-11-18 08:38:32 -0700

    updatng friend choices

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Carol",
+		photo: "https://thumbs.dreamstime.com/z/30s-soccer-mom-544294.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		name: "Deloris",
+		photo: "https://s3.amazonaws.com/media.lifeisgood.com/photo/2014/04/23/mom2_1398272713908_10901_ver1.0.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Crazy Fred",
+		photo: "https://assets3.thrillist.com/v1/image/1299823/size/tmg-article_default_mobile.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Corn Psychic",
+		photo: "https://i.pinimg.com/736x/37/9e/d1/379ed1453f57681105a4edfc9b17a3f5--photos-google-stock-photos.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Narcissist",
+		photo: "https://www.freegreatpicture.com/files/94/28351-business-people-stock.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
